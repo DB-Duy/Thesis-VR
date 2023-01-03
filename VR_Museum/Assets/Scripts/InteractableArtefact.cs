29f3d3f59f90c5317a84c4cc46c72fac69aecdc5
duydb4 <duydb4@vng.com.vn>
2023-01-03 18:42:07 +0700

    added rotation

--- VR_Museum/Assets/Scripts/InteractableArtefact.cs
+++ VR_Museum/Assets/Scripts/InteractableArtefact.cs
@@ -9,7 +9,9 @@ using UnityEngine.XR.Interaction.Toolkit.Transformers;
 public struct OriginalTransform
 {
     public Vector3 originalPosition;
+
     public Vector3 originalScale;
+
     public Vector3 originalRotation;
 }
 
@@ -17,11 +19,19 @@ public class InteractableArtefact : MonoBehaviour
 {
     [SerializeField]
     private XRGrabInteractable _interactable;
+
     public OriginalTransform OriginalTransform { get; private set; }
+
     private float _initialDistance = 0;
 
     [SerializeField]
+    private bool _isRotating = false;
+
+    [SerializeField]
     private GameObject _description;
+
+    [SerializeField]
+    private float _rotateSpeed = 1f;
 
     private void OnValidate()
     {
@@ -33,14 +43,26 @@ public class InteractableArtefact : MonoBehaviour
         StoreInformation();
     }
 
+    private void Update()
+    {
+        RotateOnUpdate();
+    }
+
+    private void RotateOnUpdate()
+    {
+        if (!_isRotating || _interactable.isSelected) return;
+        transform
+            .Rotate(new Vector3(0f, 30f, 0f) * _rotateSpeed * Time.deltaTime);
+    }
+
     private void StoreInformation()
     {
-        OriginalTransform = new OriginalTransform
-        {
-            originalPosition = transform.position,
-            originalScale = transform.localScale,
-            originalRotation = transform.rotation.eulerAngles
-        };
+        OriginalTransform =
+            new OriginalTransform {
+                originalPosition = transform.position,
+                originalScale = transform.localScale,
+                originalRotation = transform.rotation.eulerAngles
+            };
     }
 
     public void ResetObjectTransform()
@@ -49,15 +71,20 @@ public class InteractableArtefact : MonoBehaviour
         {
             return;
         }
-        _interactable.transform.SetPositionAndRotation(
-            OriginalTransform.originalPosition,
-            Quaternion.Euler(OriginalTransform.originalRotation)
-        );
+        _interactable
+            .transform
+            .SetPositionAndRotation(OriginalTransform.originalPosition,
+            Quaternion.Euler(OriginalTransform.originalRotation));
         _interactable.transform.localScale = OriginalTransform.originalScale;
     }
 
     public void ToggleDescription()
     {
         _description.SetActive(!_description.activeSelf);
+    }
+
+    public void ToggleObjectRotation()
+    {
+        _isRotating = !_isRotating;
     }
 }
